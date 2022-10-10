97598df217aea577fcf28ec7d0ee66805dca2da1
bluebeeweb <treidyoung@gmail.com>
2022-10-10 00:54:40 -0400

    Added TODO for guard and for redirects in app-routing.module

--- address-book/src/app/app-routing.module.ts
+++ address-book/src/app/app-routing.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-// TODO: add guard(s)
-
+// TODO: add guard(s) for routes
 const routes: Routes = [
   {
     path: 'address-book',
     loadChildren: () => import('./address-book/address-book.module').then(m => m.AddressBookModule)
   },
+  // TODO: add login and error components
   { path: '', redirectTo: 'address-book', pathMatch: 'full' },
   { path: '**', redirectTo: 'address-book', pathMatch: 'full'},
 ];
